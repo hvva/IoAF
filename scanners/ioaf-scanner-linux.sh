f3864f6d73af5c1cb90e5c69e5e55e09d8adad5b
Joshua James <hvva16@gmail.com>
2014-04-28 12:28:24 +0900

    Other OS disk to SQLite DB scanners initial commit

---
+++ scanners/ioaf-scanner-linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# A linux file meta-data scanner
+# Input: a suspect Linux system (Live or Disk Image)
+# Output: an SQLite database in the IoAF Sig.db format
---
+++ scanners/ioaf-scanner-osx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# An OSx file meta-data scanner
+# Input: a suspect OSx system (Live or Disk Image)
+# Output: an SQLite database in the IoAF Sig.db format
